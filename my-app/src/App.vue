196f54d1e9beb54b4f685bd41fb3eed2e38fb844
Angie <angietea101@gmail.com>
2024-07-15 05:10:56 -0700

    WIP: Sidebar

---
+++ my-app/src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import Sidebar from './components/Sidebar.vue';
+</script>
+
+<template>
+  <div class="app">
+    <Sidebar />
+     <router-view />
+  </div>
+</template> 
+
+<style lang="scss">
+:root {
+  --darkdark: #1a1625;
+  --medium-dark: #2f2b3a;
+  --light-dark: #46424f;
+  --light: #908d96;
+  --background: #121212;
+  --sidebar-width: 80px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+  color: rgba(255, 255, 255, 0.87);
+}
+
+body {
+  background: var(--medium-dark);
+}
+
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+
+.app {
+  display: flex;
+
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+
+    @media (max-width) {
+      padding-left: 6rem;
+    }
+  }
+}
+</style>
---
+++ my-app/src/components/Sidebar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <aside>
+        Sidebar
+    </aside>
+</template>
+
+<style lang="scss" scoped>
+aside {
+    display: flex;
+    flex-direction: column;
+    width: var(--sidebar-width);
+    overflow: hidden;
+    min-height: 100vh;
+    padding: .5rem;
+
+    background-color: var(--darkdark);
+
+    @media (max-width: 768px) {
+        position: fixed;
+        z-index: 99;
+    }
+}
+</style>
